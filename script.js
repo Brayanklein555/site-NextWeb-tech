86b5e2ef4e489c271d955b5d84c989ca01ba2c1c
Brayanklein555 <brayanprogramacao@gmail.com>
2026-01-08 19:13:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", e => {
+    const href = link.getAttribute("href");
+    if (href && !href.startsWith("#")) {
+      e.preventDefault();
+      document.body.classList.add("fade-out");
+      setTimeout(() => window.location.href = href, 400);
+    }
+  });
+});
